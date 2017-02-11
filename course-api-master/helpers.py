2ee0ae97f7532d754f6006c23ac3ec5c52cc3535
Kai Wen Wang <wangkaiwen998@yahoo.ca>
2017-02-10 21:49:41 -0500

    fixed global issue with helpers

--- course-api-master/helpers.py
+++ course-api-master/helpers.py
@@ -1,60 +1,53 @@
 import json
 
+class ScottyLabsHandler(object):
+    def __init__(self):
+        self.json_file = open('out.json')
+        self.json_str = self.json_file.read()
+        self.json_data = json.loads(self.json_str)
+        self.courses = self.json_data['courses']
 
-json_file = open('out.json')
-json_str = json_file.read()
-json_data = json.loads(json_str)
+    def getPrereqs(self,courses,courseName):
+        return courses[courseName]['prereqs']
 
-courses = json_data['courses']
+    def getCoreqs(self,courses,courseName):
+        return courses[courseName]['coreqs']
 
+    #separated by ands/ OR courses are in 2d list
+    def getPreArray(self,courses,courseName):
+        if self.getPrereqs(courses,courseName) == None: return None
+        pre = self.removeParenthesAndWhite(self.getPrereqs(courses,courseName))
+        pre = pre.split('and')
+        for i in range(len(pre)):
+            pre[i] = pre[i].split('or')
+        return pre
 
+    def getCoArray(self,courses,courseName):
+        co = self.removeParenthesAndWhite(getCoreqs(courses,courseName))
+        co = co.split('and')
+        for i in range(len(co)):
+            co[i] = co[i].split('or')
+        return co
 
-def getPrereqs(courses,courseName):
-    return courses[courseName]['prereqs']
+    def removeParenthesAndWhite(self,s):
+        s = s.replace("(", "")
+        s = s.replace(")", "")
+        s = s.replace(" ", "")
+        return s
 
-def getCoreqs(courses,courseName):
-    return courses[courseName]['coreqs']
-
-#separated by ands/ OR courses are in 2d list
-def getPreArray(courses,courseName):
-    if getPrereqs(courses,courseName) == None: return None
-    pre = removeParenthesAndWhite(getPrereqs(courses,courseName))
-    pre = pre.split('and')
-    for i in range(len(pre)):
-        pre[i] = pre[i].split('or')
-    return pre
-
-def getCoArray(courses,courseName):
-    co = removeParenthesAndWhite(getCoreqs(courses,courseName))
-    co = co.split('and')
-    for i in range(len(co)):
-        co[i] = co[i].split('or')
-    return co
-
-def removeParenthesAndWhite(s):
-    s = s.replace("(", "")
-    s = s.replace(")", "")
-    s = s.replace(" ", "")
-    return s
-
-def prereqsFuture(courses,courseName):
-    result = set()
-    for course in courses:
-        #reqs = course["prereqs_obj"]["reqs_list"]
-        reqs = getPreArray(courses,course)
-        if reqs == None:
-            continue
-        print(course,reqs,"hello")
-        for prereqs in reqs:
-            if courseName in prereqs:
-                result.add(course)
-    return result
+    def prereqsFuture(self,courses,courseName):
+        result = list()
+        for course in courses:
+            reqs = self.getPreArray(courses,course)
+            if reqs == None:
+                continue
+            for prereqs in reqs:
+                if courseName in prereqs:
+                    result.append(course)
+        return result
         
 
-
-# print(getPreArray(courses,"15-150"))
-# print(getCoArray(courses,"15-122"))
-# print(getPreArray(courses,"10-601"))
-# print(prereqsFuture(courses,"15-112"))
+handler = ScottyLabsHandler()
+print(handler.prereqsFuture(handler.courses,"15-112"))
 
 
