4d9e025d95b94a1f0d27a167f4fd1874c6a85386
yshen-1 <yuyi.shen3@gmail.com>
2017-02-10 21:41:34 -0500

    Fixed spaces

--- course-api-master/courseNode.py
+++ course-api-master/courseNode.py
@@ -1,19 +1,19 @@
 class courseNode(object):
     def __init__(self,courseName):
         self.courseName=courseName
-	self.x = 0
-	self.y = 0
-	self.r = 30
-	self.prereqsFor = set()
-	self.prereqsNeeded = set()
+        self.x = 0
+        self.y = 0
+        self.r = 30
+        self.prereqsFor = set()
+        self.prereqsNeeded = set()
 
     def setPosition(self,x,y):
-	self.x = x
-	self.y = y 
+        self.x = x
+        self.y = y 
 
     def addPrereqsFor(self,L):
-	self.prereqsFor=self.prereqsFor.union(set(L))
+        self.prereqsFor=self.prereqsFor.union(set(L))
                 
     def addPrereqsNeeded(self,L):
-	self.prereqsNeeded=self.prereqsNeeded.union(set(L))
+        self.prereqsNeeded=self.prereqsNeeded.union(set(L))
 
